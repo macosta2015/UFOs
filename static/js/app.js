b866644c928a430a09c9a7d106dfe3aff1c0717f
macosta2015 <macosta2015@my.fit.edu>
2021-10-04 00:04:08 -0400

    update files

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,18 @@
 //A file in our repo folder names app.js 
 //We will keep the code that builds the HTML and fills it with data from data.js
 
+// import the data from data.js
+const tableData = data;
+
+// Reference the HTML table using d3
+var tbody = d3.select("tbody");
+
+// Simple JavaScript console.log statement
+function printHello(){
+    console.log("Hello there!")
+};
+
+
+//some JavaScript runs
+//console.log(addition(4, 5));
+//addition(4, 5);
